2f64db9eb2cb3345542819aba6f247827edbfd51
Engel Pinto <engeljavierpinto@gmail.com>
2021-05-13 19:45:18 -0400

    refactor: Cambié a ChoiceFilter el atributo status del filter.

--- apps/comics/infrastructure/dj/comicsapp/filters.py
+++ apps/comics/infrastructure/dj/comicsapp/filters.py
@@ -1,10 +1,11 @@
 from django_filters import rest_framework as filters
 
 from .models import Comic
+from .models import COMIC_STATUS
 
 
 class ComicFilter(filters.FilterSet):
-    status = filters.CharFilter(field_name="status")
+    status = filters.ChoiceFilter(choices=COMIC_STATUS)
 
     class Meta:
         model = Comic
